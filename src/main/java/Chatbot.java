72950633d980e11c558fbdb7a7b0eef98cf722c1
Gauri Mutha <gvm160030@utdallas.edu>
2019-01-05 17:16:09 -0600

    Basic code for resource paths

--- src/main/java/Chatbot.java
+++ src/main/java/Chatbot.java
@@ -1,2 +1,25 @@
+import java.io.File;
+
 public class Chatbot {
+    private static final boolean TRACE_MODE = false;
+    static String botName = "chatty";
+
+    public static void main(String[] args) {
+        try{
+            String resourcePath = getResourcePath();
+
+
+        }catch(Exception e){
+
+        }
+    }
+
+    private static String getResourcePath() {
+        File currDir = new File(".");
+        String path = currDir.getAbsolutePath();
+        path = path.substring(0, path.length() - 2);
+        System.out.println(path);
+        String resourcePath = path + File.separator + "src" + File.separator + "main" +File.separator + "resources";
+        return resourcePath;
+    }
 }
