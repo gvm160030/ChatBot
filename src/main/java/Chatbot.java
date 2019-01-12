5e448d802c2618275f01c4bd67536d25d9a1d85f
Gauri Mutha <gvm160030@utdallas.edu>
2019-01-11 20:50:05 -0600

    Modified code for working of the chatbot

--- src/main/java/Chatbot.java
+++ src/main/java/Chatbot.java
@@ -1,16 +1,48 @@
+import org.alicebot.ab.*;
+import org.alicebot.ab.utils.IOUtils;
+
 import java.io.File;
+/*
+Reference : https://howtodoinjava.com/ai/java-aiml-chatbot-example/
+ */
 
 public class Chatbot {
     private static final boolean TRACE_MODE = false;
-    static String botName = "chatty";
 
     public static void main(String[] args) {
         try{
             String resourcePath = getResourcePath();
+            System.out.println(resourcePath);
+            MagicBooleans.trace_mode = TRACE_MODE;
+            Bot bot = new Bot("super",resourcePath);
+            Chat chatSession = new Chat(bot);
+            bot.brain.nodeStats();
+            String textLine = "";
 
-
+            while(true) {
+                System.out.print("Human : ");
+                textLine = IOUtils.readInputTextLine();
+                if ((textLine == null) || (textLine.length() < 1))
+                    textLine = MagicStrings.null_input;
+                if (textLine.equals("q")) {
+                    System.exit(0);
+                } else if (textLine.equals("wq")) {
+                    bot.writeQuit();
+                    System.exit(0);
+                } else {
+                    String request = textLine;
+                    if (MagicBooleans.trace_mode)
+                        System.out.println("STATE=" + request + ":THAT=" + ((History) chatSession.thatHistory.get(0)).get(0) + ":TOPIC=" + chatSession.predicates.get("topic"));
+                    String response = chatSession.multisentenceRespond(request);
+                    while (response.contains("&lt;"))
+                        response = response.replace("&lt;", "<");
+                    while (response.contains("&gt;"))
+                        response = response.replace("&gt;", ">");
+                    System.out.println("Robot : " + response);
+                }
+            }
         }catch(Exception e){
-
+            System.out.println(e.getCause());
         }
     }
 
